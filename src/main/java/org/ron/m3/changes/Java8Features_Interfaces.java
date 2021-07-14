d10509626cc89345dfdc4844f7229e8630ad895d
ron <ron.lobo@yahoo.com>
2021-07-14 15:33:51 +0100

    0024: implementation in interfaces

---
+++ src/main/java/org/ron/m3/changes/Java8Features_Interfaces.java
@@ -0,0 +1,73 @@
+package org.ron.m3.changes;
+
+import java.time.LocalDateTime;
+
+public class Java8Features_Interfaces {
+
+	public static void main(String[] args) {
+		Utils.print("Features Introduced in Java 8: Interfaces()");
+
+		Java8Interface.hiWorld("new interface");
+	}
+
+	public interface Java8Interface {
+
+		// static methods - these must be defined in the interface and cannot be overridden
+		static String hiWorld(String s) {
+			return Utils.print("hi", s, "world");
+		}
+
+		void method(String str);
+
+		void method(int i);
+
+		void method(double d, boolean b);
+
+		// A method with an implementation (which can be overridden).
+		default void log(String str, int level) {
+			LocalDateTime now = LocalDateTime.now();
+			Utils.print(now, "Level", level, ":", str);
+		}
+
+//        default String toString() {
+//            return "These are not the defaults you are looking for";
+//        }
+	}
+
+
+	@FunctionalInterface
+	private interface Hello {
+		String hi();
+	}
+
+
+	@FunctionalInterface
+	public interface Greeting {
+		void hi();
+	}
+
+	public interface USGreeting extends Greeting {
+		@Override
+		default void hi() {
+			System.out.println("howdy");
+		}
+
+		void m1();
+
+		void m2();
+	}
+
+	public interface OzGreeting extends Greeting {
+		default void hi() {
+			System.out.println("g'day");
+		}
+	}
+
+	public interface MyGreeting extends USGreeting, OzGreeting {
+		default void hi() {
+			USGreeting.super.hi();
+			OzGreeting.super.hi();
+			System.out.println("sup");
+		}
+	}
+}
